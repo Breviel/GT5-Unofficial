4f856748988c457b4426e14cc588709c3700b6ff
BlueWeabo <ilia.iliev@tuta.com>
2024-08-04 10:14:07 +0300

    structures, power, logic, tooltips

--- src/main/java/gregtech/api/enums/Mods.java
+++ src/main/java/gregtech/api/enums/Mods.java
@@ -112,6 +112,7 @@ public enum Mods {
     Minecraft(Names.MINECRAFT),
     MineTweaker(Names.MINE_TWEAKER),
     MobsInfo(Names.MOBS_INFO),
+    MuTECore(Names.MUTECORE),
     NEICustomDiagrams(Names.N_E_I_CUSTOM_DIAGRAMS),
     NEIOrePlugin(Names.N_E_I_ORE_PLUGIN),
     Natura(Names.NATURA),
@@ -291,6 +292,7 @@ public enum Mods {
         public static final String MINECRAFT = "minecraft";
         public static final String MINE_TWEAKER = "MineTweaker3";
         public static final String MOBS_INFO = "mobsinfo";
+        public static final String MUTECORE = "mutecore";
         public static final String N_E_I_CUSTOM_DIAGRAMS = "neicustomdiagram";
         public static final String N_E_I_ORE_PLUGIN = "gtneioreplugin";
         public static final String NATURA = "Natura";
